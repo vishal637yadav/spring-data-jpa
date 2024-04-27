3331e9fcfb91bb2a58cdc9cc1374dc9c52087e06
vishal637yadav <vishal637yadav@gmail.com>
2024-04-27 20:04:46 +0530

    Reverting RevisionRepository addition

--- src/main/java/com/sourcecode/springdatajpa/repository/BookInfoRepo.java
+++ src/main/java/com/sourcecode/springdatajpa/repository/BookInfoRepo.java
@@ -2,7 +2,6 @@ package com.sourcecode.springdatajpa.repository;
 
 import com.sourcecode.springdatajpa.model.BookInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
